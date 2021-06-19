cc9829cb46e84eb6a589f6a353a665f6ff777242
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-06-19 16:24:01 +0300

    Update week3.sql

--- week3.sql
+++ week3.sql
@@ -1,4 +1,3 @@
-
 -- Подзаявки(заявка, която е част от друга подзаявка, ограждат се със ())
 /* Връщат:
 
@@ -22,7 +21,24 @@ WHERE starname IN (SELECT name
  FROM MovieStar
  WHERE gender = 'F');
 
+use pc
+--всички принтери с най-висока цена(тоест може да имаме няколко принтера с най-висока цена и искаме да ги изведем)
+select *
+from printer 
+where price >= all(select price from printer)
+--да вземем всички възможни цени и да намерим такава, която е >= на всяка от тези цени
+--за всяко от числата от (400,270,...) сравняваме дали 400 е >= от всички тн за другите.
 
---всички принтери с най-висока цена
+
+--Точно един принтер с най-висока цена
+
+select top 1 *
+from printer
+order by price desc
+
+select top 1*
+from printer
+where price >= all(select price from printer)
+
 
 
