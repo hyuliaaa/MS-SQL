8354c7dd8c18cf53c7c84f8be970b7ca2880edc9
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-06-21 23:00:39 +0300

    Update week5.sql

--- week5.sql
+++ week5.sql
@@ -77,6 +77,7 @@ from STARSIN
 select name, COUNT(STARNAME)
 from MOVIESTAR
 left join STARSIN on name=STARNAME
+
 group by NAME
 
 --Да се изведе средният брой филми, в които са се снимали актьорите
@@ -88,3 +89,115 @@ select name, COUNT(STARNAME)  as movieNumber
 from MOVIESTAR
 left join STARSIN on name=STARNAME
 group by NAME ) stat
+
+
+
+use pc
+--Напишете заявка, която извежда средната честота на процесорите на компютрите.
+
+
+
+select avg(speed)
+from pc
+
+--Напишете заявка, която за всеки производител извежда средния
+--размер на екраните на неговите лаптопи.
+
+select maker, avg(screen)
+from laptop
+join product on laptop.model=product.model
+group by maker
+
+--Напишете заявка, която извежда средната честота на лаптопите с цена над 1000.
+
+select avg(speed)
+from laptop
+where price > 1000
+
+--Напишете заявка, която извежда средната цена на компютрите и
+--лаптопите на производител ‘B’ (едно число).
+
+select avg(price)
+from
+(
+	select price
+	from product p join pc
+	on p.model=pc.model 
+	where maker='B'
+
+	union all
+
+	select price
+	from product p join laptop l
+	on p.model=l.model 
+	where maker='B'
+) AllPrices;
+
+
+--Напишете заявка, която извежда средната цена на компютрите
+--според различните им честоти на процесорите.
+
+select *from pc
+
+select speed,avg(price)
+from pc
+group by speed
+
+--Напишете заявка, която извежда производителите, които са
+--произвели поне по 3 различни модела компютъра.
+
+select *
+from product
+
+select maker
+from product
+where type='PC'
+group by maker
+having count(*)>=3
+
+--Напишете заявка, която извежда производителите на компютрите с най-висока цена.
+select *
+from product
+
+select distinct maker
+from product
+join pc on product.model=pc.model
+where price= (select max(price) from pc)
+
+
+--Напишете заявка, която извежда средната цена на компютрите за
+--всяка честота, по-голяма от 800 MHz.
+
+select speed,avg(price)
+from pc
+where speed > 800
+group by speed
+
+--Напишете заявка, която извежда средния размер на диска на тези
+--компютри, произведени от производители, които произвеждат и принтери.
+
+
+select avg(hd)
+from pc 
+join product p on p.model=pc.model
+where maker in
+	(select maker
+	from product
+	where type='Printer');
+
+--Напишете заявка, която за всеки размер на лаптоп намира
+--разликата в цената на най-скъпия и най-евтиния лаптоп със същия
+--размер.
+
+
+select *from laptop
+
+
+select screen,max(price)-min(price)
+from laptop
+group by screen
+
+
+
+
+
