740181989bc56fa29ac121e378af76062dee61d3
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-06-22 19:21:49 +0300

    Update week4.sql

--- week4.sql
+++ week4.sql
@@ -35,3 +35,77 @@ where OUTCOMES.ship is null
 --участвали кораби от този клас. Ако даден клас няма кораби или има, но те не са
 --участвали в битка, също да се изведат (със стойност NULL за име на битката).
 
+select distinct classes.class, battle
+from outcomes
+join ships on ship = name
+right join classes
+ on ships.class = classes.class
+where country = 'Gt.Britain';
+
+--Напишете заявка, която за всеки филм,
+--по-дълъг от 120 минути, извежда
+--заглавие, година, име и адрес на студио.
+
+
+select TITLE,YEAR,STUDIONAME,ADDRESS
+from movie
+join STUDIO on STUDIONAME=NAME
+where LENGTH > 120
+
+-- Напишете заявка, която извежда името
+--на студиото и имената на актьорите,
+--участвали във филми, произведени от
+--това студио, подредени по име на студио.
+
+select distinct STUDIONAME, STARNAME
+from MOVIE
+join STARSIN on TITLE=MOVIETITLE and YEAR=MOVIEYEAR
+order by STUDIONAME
+
+--Напишете заявка, която извежда имената
+--на продуцентите на филмите, в които е
+--играл Harrison Ford.
+
+select distinct NAME
+from STARSIN
+join movie on TITLE=MOVIETITLE and YEAR=MOVIEYEAR
+join MOVIEEXEC on CERT#=PRODUCERC#
+where STARNAME='Harrison Ford'
+
+--Напишете заявка, която извежда имената
+--на актрисите, играли във филми на MGM.
+
+select NAME
+from MOVIESTAR
+join STARSIN on NAME=STARNAME
+join movie on TITLE=MOVIETITLE and YEAR=MOVIEYEAR
+where GENDER='F'  and STUDIONAME = 'MGM'
+
+--Напишете заявка, която извежда името
+--на продуцента и имената на филмите,
+--продуцирани от продуцента на ‘Star Wars’.
+
+select name, TITLE
+from MOVIEEXEC
+join movie on CERT#=PRODUCERC#
+where name=(select NAME
+			from MOVIEEXEC
+			join movie on CERT#=PRODUCERC#
+			where TITLE='Star Wars')
+ 
+ 
+ 
+--Напишете заявка, която извежда имената на актьорите, които не са участвали в нито един филм.  
+select NAME
+from MOVIESTAR
+left join STARSIN on name=STARNAME
+where MOVIETITLE is null
+
+-- OR:
+select name
+from moviestar
+where name not in (select starname from starsin);
+
+
+
+
